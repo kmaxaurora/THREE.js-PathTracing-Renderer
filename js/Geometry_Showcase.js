a6aa0642e899f5f38ae0e50ec6f562e08f8a42a8
Kyle Maxwell <kmaxwell@aurorasolar.com>
2023-10-18 10:59:18 -0600

    formatting

--- js/Geometry_Showcase.js
+++ js/Geometry_Showcase.js
@@ -2,59 +2,58 @@
 let torusObject;
 
 // called automatically from within initTHREEjs() function (located in InitCommon.js file)
-function initSceneData()
-{
-	demoFragmentShaderFileName = 'Geometry_Showcase_Fragment.glsl';
+function initSceneData() {
+  demoFragmentShaderFileName = 'Geometry_Showcase_Fragment.glsl';
 
-	// scene/demo-specific three.js objects setup goes here
-	sceneIsDynamic = false;
-	
-	cameraFlightSpeed = 60;
+  // scene/demo-specific three.js objects setup goes here
+  sceneIsDynamic = false;
 
-	// pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
-	pixelRatio = mouseControl ? 0.75 : 0.8;
+  cameraFlightSpeed = 60;
 
-	EPS_intersect = 0.01;
+  // pixelRatio is resolution - range: 0.5(half resolution) to 1.0(full resolution)
+  pixelRatio = mouseControl ? 0.75 : 0.8;
 
-	// Torus Object
-	torusObject = new THREE.Object3D();
-	pathTracingScene.add(torusObject);
+  EPS_intersect = 0.01;
 
-	torusObject.rotation.set((Math.PI * 0.5) - 0.05, -0.05, 0);
-	torusObject.position.set(-60, 6, 50);
-	torusObject.scale.set(11.5, 11.5, 11.5);
+  // Torus Object
+  torusObject = new THREE.Object3D();
+  pathTracingScene.add(torusObject);
 
-	// set camera's field of view
-	worldCamera.fov = 60;
-	focusDistance = 130.0;
+  torusObject.rotation.set(Math.PI * 0.5 - 0.05, -0.05, 0);
+  torusObject.position.set(-60, 6, 50);
+  torusObject.scale.set(11.5, 11.5, 11.5);
 
-	// position and orient camera
-	cameraControlsObject.position.set(0, 20, 120);
-	///cameraControlsYawObject.rotation.y = 0.0;
-	// look slightly downward
-	///cameraControlsPitchObject.rotation.x = -0.4;
+  // set camera's field of view
+  worldCamera.fov = 60;
+  focusDistance = 130.0;
 
+  // position and orient camera
+  cameraControlsObject.position.set(0, 20, 120);
+  ///cameraControlsYawObject.rotation.y = 0.0;
+  // look slightly downward
+  ///cameraControlsPitchObject.rotation.x = -0.4;
 
-	// scene/demo-specific uniforms go here
-	pathTracingUniforms.uTorusInvMatrix = { value: new THREE.Matrix4() };
-
+  // scene/demo-specific uniforms go here
+  pathTracingUniforms.uTorusInvMatrix = { value: new THREE.Matrix4() };
 } // end function initSceneData()
 
-
-
 // called automatically from within the animate() function (located in InitCommon.js file)
-function updateVariablesAndUniforms()
-{
-	// TORUS
-	torusObject.updateMatrixWorld(true); // 'true' forces immediate matrix update
-	pathTracingUniforms.uTorusInvMatrix.value.copy(torusObject.matrixWorld).invert();
+function updateVariablesAndUniforms() {
+  // TORUS
+  torusObject.updateMatrixWorld(true); // 'true' forces immediate matrix update
+  pathTracingUniforms.uTorusInvMatrix.value.copy(torusObject.matrixWorld).invert();
 
-
-	// INFO
-	cameraInfoElement.innerHTML = "FOV: " + worldCamera.fov + " / Aperture: " + apertureSize.toFixed(2) + " / FocusDistance: " + focusDistance + "<br>" + "Samples: " + sampleCounter;
-
+  // INFO
+  cameraInfoElement.innerHTML =
+    'FOV: ' +
+    worldCamera.fov +
+    ' / Aperture: ' +
+    apertureSize.toFixed(2) +
+    ' / FocusDistance: ' +
+    focusDistance +
+    '<br>' +
+    'Samples: ' +
+    sampleCounter;
 } // end function updateVariablesAndUniforms()
-
-
 
 init(); // init app and start animating
